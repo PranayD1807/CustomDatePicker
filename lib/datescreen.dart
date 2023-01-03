d3ff34c7980186d69fbe6433e6fa2f1615dde88c
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-01-04 00:24:56 +0530

    Added Color Options

--- lib/datescreen.dart
+++ lib/datescreen.dart
@@ -1,9 +1,5 @@
-import 'package:datepickerdemo/widget/custom_date_picker.dart';
 import 'package:datepickerdemo/widget/date_picker_textfield.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class DateScreen extends StatefulWidget {
   const DateScreen({super.key});
